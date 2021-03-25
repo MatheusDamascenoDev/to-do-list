8d2309ff40afcc7cf3b577403647399a99f23e46
Matheus Damasceno <matheusdjaques@gmail.com>
2021-03-25 04:50:37 -0300

    :rocket: Challenge Completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,32 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks((oldState: any) => [...oldState, newTask]);
+    setNewTaskTitle('');
+    
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map((task: { id: number; isComplete: boolean; }) => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } :task);
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter((task: { id: number; }) => task.id !== id);
+
+    setTasks(filteredTasks)
   }
 
   return (
