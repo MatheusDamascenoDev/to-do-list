0d57915ab6aee940d3c9e32102703bdd91b1e74b
Matheus Damasceno Jaques <matheusdjaques@gmail.com>
2023-04-25 10:55:33 -0300

    Update index.tsx

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { StoreProvider } from './Store'
-import { HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <StoreProvider>
-      <HelmetProvider>
         <App />
-      </HelmetProvider>
     </StoreProvider>
   </React.StrictMode>,
 )
